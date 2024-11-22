2af3c81233b16a4021e788faf3813b53b5bb3d2f
m1a2023 <mag2017go@gmail.com>
2024-11-22 07:42:45 +0500

    half

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,10 @@
 package com.example.task02;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +13,29 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+
+        changeToUnix(System.in, System.out);
+    }
+
+    public static void changeToUnix(InputStream inputStream, OutputStream outputStream)
+            throws IOException {
+        if (inputStream == null && outputStream != null)
+            throw new IllegalArgumentException();
+
+        byte current, previous = -1;
+
+        while ((current = (byte) inputStream.read()) != -1) {
+            if (previous == 13 && current == 10) {
+                outputStream.write(current);
+                previous = -1;
+            } else {
+                if (previous != -1) outputStream.write(previous);
+                previous = current;
+            }
+        }
+
+        if (previous != -1) outputStream.write(previous);
+
+        outputStream.flush();
     }
 }
